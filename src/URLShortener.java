41edd56055190ed466826ae5571b9e6c6b1b3e9e
nick-kalash777 <nick.kalash777@gmail.com>
2025-01-12 05:52:11 +0300

    added to main

--- src/URLShortener.java
+++ src/URLShortener.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
