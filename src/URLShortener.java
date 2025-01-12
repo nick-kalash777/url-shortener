7a035bc199e519daa97a8faefd40cbad24babf5a
nick-kalash777 <nick.kalash777@gmail.com>
2025-01-12 05:51:35 +0300

    added to main

--- src/URLShortener.java
+++ src/URLShortener.java
@@ -21,7 +21,7 @@ public class URLShortener {
             "/limit", new Commands.UseLimit());
 
 
-    public static void main(String[] args) {
+    public static void run() {
         System.out.println("""
                 Link Shortener v1.0.\
 
